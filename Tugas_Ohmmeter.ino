c6af53654051da0710e2a90d3ca36bfbe669266a
Nashrullahabdurrahmanhanif18 <Nashrullahabdurrahmanhanif@yahoo.co.id>
2016-11-29 14:45:03 +0700

    -
    -

---
+++ Tugas_Ohmmeter.ino
@@ -0,0 +1,24 @@
+
+int pinAnalog =0;
+float data;
+float Vr1;
+float Vr2;
+float R2;
+float R1 = 10;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  data = analogRead(pinAnalog);
+  Vr2 = data *5/1024;
+  Vr1 = 5 - Vr2;
+  R2 = (Vr2*R1)/(Vr1);
+  Serial.print("Voltage :");
+  Serial.println(Vr1);
+  Serial.print("Resistance :");
+  Serial.println(R2);
+  delay(1000);
+  }
+
